0a5254c937388f1632739ffdce32fb1b84920676
Bella Cha <cha.maria.belen@gmail.com>
2020-11-05 00:24:04 +0100

    Add endpoints

--- app.py
+++ app.py
@@ -16,22 +16,51 @@ def index():
 def get_teams():
     from models import Team
     teams = Team.query.all()
-    names = {}
-    for idx, team in enumerate(teams):
-      names.update({ idx + 1: team.name })
 
-    return make_response(jsonify(names), 200)
+    teams_info = []
+    for team in teams:
+      teams_info.append({'id': team.id, 'name': team.name, 'score': team.score})
 
-@app.route('/selected_word/', methods=['POST'])
+    return make_response(jsonify(teams_info), 200)
+
+@app.route('/select_player/', methods=['GET'])
+def get_player():
+    from models import Team
+    from models import Word
+    import random
+    team_id = request.args.get('team_id')
+
+    team = Team.query.get(team_id)
+    players = team.players
+
+    amount_of_words = 0
+    random_player = random.choice(players)
+    selected_player = {'id': random_player.id, 'name': random_player.name}
+
+    for player in players:
+      player_words = len(player.words)
+      if player_words > amount_of_words:
+        amount_of_words = player_words
+        selected_player = {'id': player.id, 'name': player.name}
+
+    return make_response(jsonify(selected_player), 200)
+
+@app.route('/save_word/', methods=['POST'])
 def post_word():
-    word = request.form.get('word')
+    from models import Word
+    from models import Player
 
-    if word:
-        message = { "message": f"{word} saved" }
-        return make_response(jsonify(message), 200)
-    else:
-        message = { "message": "no word found"}
-        return make_response(jsonify(message), 404)
+    word = request.form.get('word')
+    score = request.form.get('score')
+    player_name = request.form.get('player_name')
+
+    player = Player.query.filter_by(name=player_name).first()
+
+    new_word = Word(word=word, score=score, player=player, guessed=0)
+    new_word.save
+
+    message = {"message": f"{word} saved"}
+    return make_response(jsonify(message), 200)
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
--- models.py
+++ models.py
@@ -1,24 +1,53 @@
 from app import db
+from mixins import CRUDMixin
 
-class Team(db.Model):
+class Team(CRUDMixin, db.Model):
     __tablename__ = 'teams'
-    __table_args__ = { 'extend_existing': True }
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.Text)
     score = db.Column(db.Integer)
+    words = db.relationship("Word", secondary="players")
 
-class Player(db.Model):
+
+class Player(CRUDMixin, db.Model):
     __tablename__ = 'players'
     __table_args__ = { 'extend_existing': True }
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    words = db.Column(db.Text)
-    team_id = db.Column(db.Integer)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    team = db.relationship('Team', backref=db.backref('players', lazy=True))
+
+    def __repr__(self):
+        return '<Player %r>' % self.name
+
+
+class Word(CRUDMixin, db.Model):
+    __tablename__ = 'words'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.Text)
+    score = db.Column(db.Integer, default=0)
+    guessed = db.Column(db.Boolean)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'), nullable=False)
+    player = db.relationship('Player', backref=db.backref('words', lazy=True))
+
+    def __repr__(self):
+        return '<Word %r>' % self.word
 
 # CREATE TABLE teams (
 #   id INTEGER PRIMARY KEY,
 #   name TEXT not null,
 #   score integer default 0
+# );
+
+# CREATE TABLE words(
+#     id INTEGER PRIMARY KEY,
+#     word TEXT not null,
+#     score integer not null,
+#     player_id INTEGER NOT NULL,
+#     FOREIGN KEY (player_id)
+#         REFERENCES teams (player_id)
 # );
 
 # CREATE TABLE players (
